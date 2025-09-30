8c2f391bddd8cd30578dce0cfd7b073d5323f71c
C0A24147 <c0a24147d3@edu.teu.ac.jp>
2025-09-30 14:12:13 +0900

    爆弾作った

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,4 +1,5 @@
 import os
+import random
 import sys
 import pygame as pg
 
@@ -20,6 +21,13 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+    bb_img = pg.Surface((20, 20))  # 20*20の四角
+    pg.draw.circle(bb_img, (255, 0, 0), (10,10), 10)  # 半径10の赤い円
+    bb_img.set_colorkey((0, 0, 0))  # 四隅を透明に
+    bb_rct = bb_img.get_rect()
+    bb_rct.centerx = random.randint(0, WIDTH)
+    bb_rct.centery = random.randint(0, HEIGHT)
+    vx, vy = +5, +5  # 爆弾の速度
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -44,6 +52,8 @@ def main():
         #     sum_mv[0] += 5
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+        screen.blit(bb_img, bb_rct)  # 爆弾を表示
+        bb_rct.move_ip(vx,vy)
         pg.display.update()
         tmr += 1
         clock.tick(50)
